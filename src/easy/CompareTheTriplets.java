03b40a550d4d91ca54e0a207e40f3e5d63cf7401
Abzelhan Sahitov <abzelhan.sahitov@robusta.team>
2020-01-27 00:13:08 +0600

    27.01.2020 | EASY | Solved Compare-the-triplets problem

---
+++ src/easy/CompareTheTriplets.java
@@ -0,0 +1,50 @@
+package easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class CompareTheTriplets {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> alicePoints, List<Integer> bobPoints) {
+
+        int categoryAmount = 3;
+        int aliceScore = 0;
+        int bobScore = 0;
+
+        for (int i = 0; i < categoryAmount; i++) {
+            if (alicePoints.get(i) > bobPoints.get(i)) {
+                aliceScore++;
+            } else if (alicePoints.get(i) < bobPoints.get(i)) {
+                bobScore++;
+            }
+        }
+
+        return Arrays.asList(aliceScore, bobScore);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        bufferedReader.close();
+
+        List<Integer> result = compareTriplets(a, b);
+
+        System.out.println(result.stream().map(n -> n.toString()).collect(Collectors.joining(" ")));
+    }
+}
