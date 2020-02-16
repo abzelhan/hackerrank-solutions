c236364a21582f1d5b992df1b42c1863dc3a76c8
abzelhan <abzalsahitov@gmail.com>
2020-02-16 23:54:30 +0600

    Temporal solution

--- src/algorithms/implementation/easy/CountingValleys.java
+++ src/algorithms/implementation/easy/CountingValleys.java
@@ -1,4 +1,38 @@
 package algorithms.implementation.easy;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class CountingValleys {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = countingValleys(n, s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
