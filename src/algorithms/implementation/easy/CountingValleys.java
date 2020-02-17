1e07e509169799f2feb34aa3e10d4b820b3cc6b5
abzelhan <abzalsahitov@gmail.com>
2020-02-17 23:44:43 +0600

    Solved CountingValleys problem

--- src/algorithms/implementation/easy/CountingValleys.java
+++ src/algorithms/implementation/easy/CountingValleys.java
@@ -12,14 +12,38 @@ public class CountingValleys {
 
     // Complete the countingValleys function below.
     static int countingValleys(int n, String s) {
+        int valleyCount = 0;
+        int seaLevel = 0;
+
+        boolean belowSeaLevel = false;
+
+        for (int i = 0; i < n; i++) {
+            char step = s.charAt(i);
+
+            if (step == 'U') {
+                ++seaLevel;
+            } else if (step == 'D') {
+                --seaLevel;
+            }
+
+            if (seaLevel < 0 ) {
+                belowSeaLevel = true;
+            } else if (seaLevel > 0) {
+                belowSeaLevel = false;
+            } else if (seaLevel == 0 && belowSeaLevel) {
+                valleyCount++;
+            }
 
 
+
+        }
+
+        return valleyCount;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -28,11 +52,7 @@ public class CountingValleys {
 
         int result = countingValleys(n, s);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
+        System.out.println(result);
         scanner.close();
     }
 }
