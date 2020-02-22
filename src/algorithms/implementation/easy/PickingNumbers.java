75b2a38d45c7527dc2afbc2a830b0d91aeba84c2
abzelhan <abzalsahitov@gmail.com>
2020-02-22 20:19:09 +0600

    Temp

--- src/algorithms/implementation/easy/PickingNumbers.java
+++ src/algorithms/implementation/easy/PickingNumbers.java
@@ -15,38 +15,7 @@ import static java.util.stream.Collectors.toList;
 public class PickingNumbers {
 
     public static int pickingNumbers(List<Integer> a) {
-        ArrayList<Integer> nums = new ArrayList<>();
-        a.sort(Integer::compareTo);
-        for (int i = 0; i < a.size(); i++) {
-            Integer num = a.get(i);
-            boolean canPick = false;
-            for (int j = 0; j < a.size(); j++) {
-
-                if(i != j)
-                if (Math.abs(num - a.get(j)) <= 1) {
-                    canPick = true;
-                    break;
-                }
-            }
-
-            if(canPick) {
-                if (nums.size() == 0) nums.add(num);
-                else {
-                    for (Integer validNum : nums) {
-                        if (!(Math.abs(validNum - num) <= 1)) {
-                            canPick = false;
-                            break;
-                        }
-                    }
-                    if (canPick) {
-                        nums.add(num);
-                    }
-                }
-            }
-
-        }
-
-        return nums.size();
+        ///
     }
 
     public static void main(String[] args) throws IOException {
