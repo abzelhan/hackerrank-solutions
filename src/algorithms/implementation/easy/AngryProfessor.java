8413db9820d5fdf57772ab46cb4905053878c9e3
abzelhan <abzalsahitov@gmail.com>
2020-06-18 02:01:50 +0600

    Implementation| Easy| AngryProfessor| Added url, io

--- src/algorithms/implementation/easy/AngryProfessor.java
+++ src/algorithms/implementation/easy/AngryProfessor.java
@@ -3,12 +3,15 @@ package algorithms.implementation.easy;
 import java.io.IOException;
 import java.util.Scanner;
 
+/*
+    URL: https://www.hackerrank.com/challenges/angry-professor/problem
+ */
 public class AngryProfessor {
 
     // Complete the angryProfessor function below.
     static String angryProfessor(int k, int[] a) {
         int sum = 0;
-        for (int i = 0; i < a.length ; i++) {
+        for (int i = 0; i < a.length; i++) {
             if (a[i] <= 0) {
                 sum++;
             }
@@ -18,6 +21,17 @@ public class AngryProfessor {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+    /*
+        Input:
+        2
+        4 3
+        -1 -3 4 2
+        4 2
+        0 -1 2 1
+        Output:
+        YES
+        NO
+     */
     public static void main(String[] args) throws IOException {
         int t = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
