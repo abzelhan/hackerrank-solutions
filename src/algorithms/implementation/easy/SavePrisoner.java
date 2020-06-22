64685a71c754225c1f57d7311de09ceaec08ec4c
abzelhan <abzalsahitov@gmail.com>
2020-06-22 22:46:51 +0600

    Implementation| Easy| Temp SavePrisoner Solution

---
+++ src/algorithms/implementation/easy/SavePrisoner.java
@@ -0,0 +1,53 @@
+package algorithms.implementation.easy;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * URL: https://www.hackerrank.com/challenges/save-the-prisoner/problem
+ * Input:
+ * 2
+ * 5 2 1
+ * 5 2 2
+ * Output:
+ * 2
+ * 3
+ */
+public class SavePrisoner {
+
+    static int saveThePrisoner(int n, int m, int s) {
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nms = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nms[0]);
+
+            int m = Integer.parseInt(nms[1]);
+
+            int s = Integer.parseInt(nms[2]);
+
+            int result = saveThePrisoner(n, m, s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
