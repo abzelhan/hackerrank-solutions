40c3ea92ec150eab7fc4ce44dabb8744d6b3c82a
abzelhan <abzalsahitov@gmail.com>
2021-06-30 23:22:14 +0600

    solved TrafficLights

---
+++ src/codeforces/week5/TrafficLights.java
@@ -0,0 +1,40 @@
+package codeforces.week5;
+
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TrafficLights {
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int n = scanner.nextInt();
+      List<Byte>[] vertex = new LinkedList[n];
+
+      for (int i = 0; i < n; i++) {
+         vertex[i] = new LinkedList<>();
+      }
+
+      int m = scanner.nextInt();
+
+      for (int i = 0; i < m; i++) {
+         byte a = scanner.nextByte();
+         byte b = scanner.nextByte();
+
+         a--;
+         b--;
+
+         vertex[a].add(b);
+         vertex[b].add(a);
+      }
+      PrintWriter printWriter = new PrintWriter(System.out);
+      for (int i = 0; i <n; i++) {
+         printWriter.print(vertex[i].size() + " ");
+      }
+
+      printWriter.flush();
+      printWriter.close();
+   }
+
+}
