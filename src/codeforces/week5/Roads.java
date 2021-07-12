91ec18bded010cff6816fadb16285d6e87e1106d
abzelhan <abzalsahitov@gmail.com>
2021-07-12 22:18:12 +0600

    solved Roads

---
+++ src/codeforces/week5/Roads.java
@@ -0,0 +1,30 @@
+package codeforces.week5;
+
+import java.util.Scanner;
+
+public class Roads {
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int n = scanner.nextInt();
+      int[][] arr = new int[n][n];
+
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+            arr[i][j] = scanner.nextInt();
+         }
+      }
+
+      int counter = 0;
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+            if (arr[i][j] == arr[j][i] && arr[i][j] == 1) {
+               counter++;
+            }
+         }
+      }
+
+      System.out.println(counter/2);
+   }
+
+}
