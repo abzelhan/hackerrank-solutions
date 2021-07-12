376a51e352a0cfc727bc5487dd610514c8be8062
abzelhan <abzalsahitov@gmail.com>
2021-07-13 01:17:31 +0600

    solved TurnMe

---
+++ src/codeforces/week5/TurnMe.java
@@ -0,0 +1,41 @@
+package codeforces.week5;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class TurnMe {
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int v = scanner.nextInt();
+
+      LinkedList<Integer>[] edgeList = new LinkedList[v];
+
+      for (int i = 0; i < v; i++) {
+         if (edgeList[i] == null) {
+            edgeList[i] = new LinkedList<>();
+         }
+         int edges = scanner.nextInt();
+         for (int j = 0; j < edges; j++) {
+            int mainEdge = i;
+            int anotherEdge = scanner.nextInt() - 1;
+            if (edgeList[anotherEdge] == null) {
+               edgeList[anotherEdge] = new LinkedList<>();
+            }
+            edgeList[anotherEdge].add(mainEdge + 1);
+         }
+      }
+
+      System.out.println(edgeList.length);
+
+      for (int i = 0; i < edgeList.length; i++) {
+         System.out.print(edgeList[i].size() + " ");
+         for (int j = 0; j < edgeList[i].size(); j++) {
+            System.out.print(edgeList[i].get(j) + " ");
+         }
+         System.out.println();
+      }
+
+   }
+
+}
