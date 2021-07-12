39283d0a8478830e971e2838c3757be2b4a1c7a3
abzelhan <abzalsahitov@gmail.com>
2021-07-13 00:59:18 +0600

    solved CheckForUndirected, ConvertFromAdjacenctMatrixToEdgeList

---
+++ src/codeforces/week5/CheckForUndirected.java
@@ -0,0 +1,31 @@
+package codeforces.week5;
+
+import java.util.Scanner;
+
+public class CheckForUndirected {
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      byte n = scanner.nextByte();
+
+      byte[][] arr = new byte[n][n];
+
+      for (byte i = 0; i < n; i++) {
+         for (byte j = 0; j < n; j++) {
+            arr[i][j] = scanner.nextByte();
+         }
+      }
+      for (byte i = 0; i < n; i++) {
+         for (byte j = 0; j < n; j++) {
+            if (arr[i][j] != arr[j][i]) {
+               System.out.println("NO");
+               return;
+            }
+         }
+      }
+
+      System.out.println("YES");
+   }
+
+}
---
+++ src/codeforces/week5/ConvertFromAdjacenctMatrixToEdgeList.java
@@ -0,0 +1,34 @@
+package codeforces.week5;
+
+import java.util.Scanner;
+
+public class ConvertFromAdjacenctMatrixToEdgeList {
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int v = scanner.nextInt();
+      byte[][] matrix = new byte[v][v];
+
+      for (int i = 0; i < v; i++) {
+         for (int j = 0; j < v; j++) {
+            matrix[i][j] = scanner.nextByte();
+         }
+      }
+
+      int counter = 0;
+      StringBuilder builder = new StringBuilder();
+      for (int i = 0; i < v; i++) {
+         for (int j = i + 1; j < v; j++) {
+            if (matrix[i][j] == 1) {
+               counter++;
+               builder.append((i + 1) + " " + (j + 1) + "\n");
+            }
+         }
+      }
+
+      System.out.println(counter);
+      System.out.print(builder);
+
+   }
+
+}
