a742e7c71979a798a0c061d333c79823e840720e
abzelhan <abzalsahitov@gmail.com>
2021-07-13 02:15:13 +0600

    solved OutdegreeByEdgeList, VertexOutdegree, OriginsAndDrains

---
+++ src/codeforces/week5/OutdegreeByEdgeList.java
@@ -0,0 +1,31 @@
+package codeforces.week5;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class OutdegreeByEdgeList {
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      byte v = scanner.nextByte();
+
+      LinkedList<Short>[] edgeList = new LinkedList[v];
+      short m = scanner.nextShort();
+
+      for (int i = 0; i < v; i++) {
+         edgeList[i] = new LinkedList<>();
+      }
+
+      for (int i = 0; i < m; i++) {
+         short a = (short) (scanner.nextShort() - 1);
+         short b = (short) (scanner.nextShort() - 1);
+         edgeList[a].add(b);
+         edgeList[b].add(a);
+      }
+
+      for (int i = 0; i < edgeList.length; i++) {
+         System.out.print(edgeList[i].size() + " ");
+      }
+   }
+
+}
