71701fcd5b83ab7afc819b76a52cd6488ea78fd9
abzelhan <abzalsahitov@gmail.com>
2021-07-12 22:18:04 +0600

    solved ColoredRain

---
+++ src/codeforces/week5/ColoredRain.java
@@ -0,0 +1,38 @@
+package codeforces.week5;
+
+import java.util.Scanner;
+
+public class ColoredRain {
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      byte n = scanner.nextByte();
+      byte arr[][] = new byte[n][n];
+
+      for (byte i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+            arr[i][j] = scanner.nextByte();
+         }
+      }
+
+      byte colors[] = new byte[n];
+
+      for (byte i = 0; i < n; i++) {
+         colors[i] = scanner.nextByte();
+      }
+
+      short counter = 0;
+
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+            if (arr[i][j] == arr[j][i] && arr[i][j] == 1 && colors[i] != colors[j]) {
+               counter++;
+            }
+         }
+      }
+
+      System.out.println(counter / 2);
+   }
+
+}
