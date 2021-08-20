047de4994f5c5e23680f7707f9537a461f6bdec8
abzelhan <abzalsahitov@gmail.com>
2021-08-20 23:02:21 +0600

    solved MagicDictionarySlow

--- src/leetcode/medium/MagicDictionarySlow.java
+++ src/leetcode/medium/MagicDictionarySlow.java
@@ -1,31 +1,36 @@
 package leetcode.medium;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 public class MagicDictionarySlow {
 
    class MagicDictionary {
 
-      LinkedList<String> list = new LinkedList<>();
+      HashMap<Integer, List<String>> map = new HashMap<>();
 
-      /**
-       * Initialize your data structure here.
-       */
       public MagicDictionary() {
 
       }
 
       public void buildDict(String[] dictionary) {
          for (String s : dictionary) {
-            list.add(s);
+            List<String> words = map.getOrDefault(s.length(), new LinkedList<>());
+
+            words.add(s);
+
+            map.put(s.length(), words);
          }
 
       }
 
       public boolean search(String searchWord) {
-         for (String word : list) {
+         List<String> words = map.get(searchWord.length());
 
-            if (word.length() == searchWord.length()) {
+         if (words != null) {
+            for (String word : words) {
+
                int counter = 0;
 
                for (int i = 0; i < searchWord.length(); i++) {
@@ -40,12 +45,12 @@ public class MagicDictionarySlow {
             }
          }
 
+
          return false;
       }
    }
 
    public static void main(String[] args) {
-  
 
    }
 
