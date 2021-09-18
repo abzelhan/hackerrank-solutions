7dffcb476d6468b5a3110297cac1e51c26c43dec
abzelhan <abzalsahitov@gmail.com>
2021-09-19 05:31:14 +0600

    Optimized Loud And Rich

--- src/leetcode/medium/graph/LoudAndRich.java
+++ src/leetcode/medium/graph/LoudAndRich.java
@@ -1,63 +1,42 @@
 package leetcode.medium.graph;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class LoudAndRich {
 
-    private static class Edge implements Comparable<Edge> {
-        private int vertex;
-        private int quietness;
+    private void dfs(int current, int from, LinkedList<Integer> graph[], int[] quietness, int[] vertex) {
 
-        public Edge(int vertex, int quietness) {
-            this.vertex = vertex;
-            this.quietness = quietness;
-        }
+        quietness[current] = quietness[from];
+        vertex[current] = vertex[from];
 
-        @Override
-        public int compareTo(Edge o) {
-            return Integer.compare(quietness, o.quietness);
-        }
-    }
-
-    private void dfs(int current, Edge from, LinkedList<Integer> graph[], Set<Integer> visited, Edge[] quietness) {
-        visited.add(current);
-        if(quietness[current].quietness > quietness[from.vertex].quietness) {
-            quietness[current] = quietness[from.vertex];
-        }
-        if(quietness[current].quietness > quietness[from.vertex].quietness) {
-            quietness[current] = quietness[from.vertex];
-        }
         for (Integer child : graph[current]) {
-            if (!visited.contains(child)) {
-                dfs(child, from, graph, visited, quietness);
+            if (quietness[child] > quietness[current]) {
+                dfs(child, from, graph, quietness, vertex);
             }
         }
     }
 
     public int[] loudAndRich(int[][] richer, int[] quiet) {
-        PriorityQueue<Edge> sortedEdges = new PriorityQueue<>();
-        LinkedList<Integer> graph[] = new LinkedList[quiet.length];
-        Set<Integer> visited = new HashSet<>();
-        Edge[] result = new Edge[quiet.length];
+        int[] quietness = new int[quiet.length];
+        int[] vertex = new int[quiet.length];
+        LinkedList<Integer> graph[] = new LinkedList[quietness.length];
 
         for (int i = 0; i < quiet.length; i++) {
-            Edge edge = new Edge(i, quiet[i]);
-            sortedEdges.add(edge);
             graph[i] = new LinkedList<>();
-            result[i] = edge;
+            quietness[i] = quiet[i];
+            vertex[i] = i;
         }
 
         for (int i = 0; i < richer.length; i++) {
             graph[richer[i][0]].add(richer[i][1]);
         }
 
-        while (!sortedEdges.isEmpty()) {
-            Edge edge = sortedEdges.poll();
-            dfs(edge.vertex, edge, graph, visited, result);
-            visited.clear();
+        for (int i = 0; i < quiet.length; i++) {
+            dfs(i, i, graph, quietness, vertex);
         }
 
-        return Arrays.stream(result).mapToInt(e -> e.vertex).toArray();
+        return vertex;
     }
 
     public static void main(String[] args) {
